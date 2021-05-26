2f79e0c282bff3b3d51334253c5779771213d999
matheus.evangelista <matheus.evangelista@g.globo>
2021-05-26 11:29:08 -0300

    Add Product entity and list Product data

--- core-data-app/ViewController.swift
+++ core-data-app/ViewController.swift
@@ -9,13 +9,57 @@ class ViewController: UIViewController {
         let appDelegate = UIApplication.shared.delegate as! AppDelegate
         let context = appDelegate.persistentContainer.viewContext
         
+        //saveProduct(context)
+        getAllProducts(context)
+        
+        //saveUser(context)
+        //getAllUsers(context)
+    }
+    
+    private func saveProduct(_ context: NSManagedObjectContext) {
+        let product = NSEntityDescription.insertNewObject(forEntityName: "Product", into: context)
+        
+        product.setValue("MacBook Pro 15", forKey: "bio")
+        product.setValue("White", forKey: "color")
+        product.setValue(1999.5, forKey: "price")
+        
+        do {
+            try context.save()
+            print("Dados salvos com sucesso")
+        } catch {
+            print("Erro ao salvar os dados: \(error.localizedDescription)")
+        }
+    }
+    
+    private func getAllProducts(_ context: NSManagedObjectContext) {
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Product")
+        
+        do {
+            let products = try context.fetch(request) as! [NSManagedObject]
+            
+            if products.count > 0 {
+                for product in products {
+                    guard
+                        let productBio = product.value(forKey: "bio"),
+                        let productColor = product.value(forKey: "color"),
+                        let productPrice = product.value(forKey: "price")
+                    else { return }
+                    print("Nome: \(productBio) / Cor: \(productColor) / Preço: \(productPrice)")
+                }
+            }
+        } catch {
+            print("Erro ao recuperar os usuários")
+        }
+    }
+    
+    private func saveUser(_ context: NSManagedObjectContext) {
         // create the User object entity
         let user = NSEntityDescription.insertNewObject(forEntityName: "User", into: context)
         
         // configure the object
         user.setValue("Souza", forKey: "name")
-        user.setValue(29, forKey: "age")
-        user.setValue("matheusevangelista", forKey: "login")
+        user.setValue(22, forKey: "age")
+        user.setValue("souzarj", forKey: "login")
         user.setValue("1234", forKey: "password")
         
         // save User data
@@ -27,4 +71,24 @@ class ViewController: UIViewController {
         }
     }
     
+    private func getAllUsers( _ context: NSManagedObjectContext) {
+        // create request
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "User")
+        
+        do {
+            let usuarios = try context.fetch(request) as! [NSManagedObject]
+            
+            if usuarios.count > 0 {
+                for usuario in usuarios {
+                    guard let userName = usuario.value(forKey: "name") else { return }
+                    print(userName)
+                }
+            } else {
+                print("Nenhum usuário encontrado")
+            }
+        } catch  {
+            print("Erro ao recuperar os usuários")
+        }
+
+    }
 }
